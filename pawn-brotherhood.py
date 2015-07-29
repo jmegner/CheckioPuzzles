a9db7a9782947ee3ac86c9cfa36bdb133a0a401c
jegner <jegner@slb.com>
2015-07-29 10:05:12 -0500

    improve pawn-brotherhood

--- pawn-brotherhood.py
+++ pawn-brotherhood.py
@@ -1,26 +1,37 @@
+'''
+author: Jacob Egner
+date: 2015-06-27 (update on 2015-07-29)
+island: home
+
+puzzle URLs:
+http://www.checkio.org/mission/pawn-brotherhood/
+https://github.com/Bryukh-Checkio-Tasks/checkio-mission-pawn-brotherhood
+
+for latest versions of my solutions, see my checkio solution github repo:
+https://github.com/jmegner/CheckioPuzzles
+
+'''
+
+
 def safe_pawns(pawnLocStrs):
-    boardLen = 8
-    pawnGrid = [[False] * boardLen for r in range(boardLen)]
+    pawnLocs = set()
 
     for pawnLocStr in pawnLocStrs:
         r = ord(pawnLocStr[1]) - ord('1')
         c = ord(pawnLocStr[0]) - ord('a')
-        pawnGrid[r][c] = True
+        pawnLocs.add((r, c))
 
     numSafePawns = 0
 
-    for r, row in list(enumerate(pawnGrid))[1:]:
-        for c, isPawn in enumerate(row):
-            if isPawn:
-                protectedFromLeft = c > 0 and pawnGrid[r - 1][c - 1]
-                protectedFromRight = c < boardLen - 1 and pawnGrid[r - 1][c + 1]
-
-                if protectedFromLeft or protectedFromRight:
-                    numSafePawns += 1
+    for r, c in pawnLocs:
+        if (r - 1, c - 1) in pawnLocs or (r - 1, c + 1) in pawnLocs:
+            numSafePawns += 1
 
     return numSafePawns
 
+
 if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
     assert safe_pawns({"b4", "d4", "f4", "c3", "e3", "g5", "d2"}) == 6
     assert safe_pawns({"b4", "c4", "d4", "e4", "f4", "g4", "e5"}) == 1
+
+
