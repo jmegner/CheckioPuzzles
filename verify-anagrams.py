02d5e4b8020da745d3c81f50491a2a74ede65443
Jacob Egner <jmegner@gmail.com>
2015-08-08 11:15:56 -0500

    add second solution for verify-anagrams

--- verify-anagrams.py
+++ verify-anagrams.py
@@ -11,6 +11,10 @@ https://github.com/jmegner/CheckioPuzzles
 
 import collections
 
+
+################################################################################
+# solution1
+
 def verify_anagrams(text1, text2):
     counter1 = getLetterCounter(text1)
     counter2 = getLetterCounter(text2)
@@ -22,6 +26,17 @@ def getLetterCounter(text):
     return collections.Counter(char.lower() for char in text if char.isalpha())
 
 
+################################################################################
+# solution2
+
+def verify_anagrams_shorter_way(text1, text2):
+    sortedLetters1 = ''.join(sorted(text1.lower())).strip()
+    sortedLetters2 = ''.join(sorted(text2.lower())).strip()
+    return sortedLetters1 == sortedLetters2
+
+
+################################################################################
+# in-file testing
 if __name__ == '__main__':
     assert isinstance(verify_anagrams("a", 'z'), bool), "Boolean!"
     assert verify_anagrams("Programming", "Gram Ring Mop") == True, "Gram of code"
