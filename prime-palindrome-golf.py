01b57b4c9ce7e8249bec935d7e13c3a394feff98
jegner <jegner@slb.com>
2015-08-11 18:59:40 -0500

    improve prime-palindrome-golf

--- prime-palindrome-golf.py
+++ prime-palindrome-golf.py
@@ -1,18 +1,5 @@
 def golf(n):
-    for i in range(n + 1, 99999):
-        if isPalindrome(i) and isPrime(i):
-            return i
-
-
-def isPrime(n):
-    if n < 2:
-        return False
-    for i in range(2, n // 2):
-        if n % i == 0:
-            return False
-    return True
-
-
-def isPalindrome(num):
-    numStr = str(num)
-    return numStr == numStr[::-1]
+ while 1:
+  n+=1
+  if `n`==`n`[::-1]and all(n%x for x in range(2,n)):
+   return n
