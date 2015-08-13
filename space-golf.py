189361c264153ea828cb47442176095d5c40fa25
jegner <jegner@slb.com>
2015-08-13 10:14:56 -0500

    improve space-golf

--- space-golf.py
+++ space-golf.py
@@ -1,2 +1,4 @@
-import itertools
-def golf(h):return min(sum(((c[0]-p[0])**2+(c[1]-p[1])**2)**.5 for c,p in zip(r,((0,0),)+r)) for r in itertools.permutations(h))
+c=complex
+def golf(h,p=(0,0)):
+ if h:return min(abs(c(*p)-c(*x))+golf(h-{x,},x)for x in h)
+ return 0
